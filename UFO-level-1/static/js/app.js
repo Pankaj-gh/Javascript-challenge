617b0985c76d2b35cb362e6f7951ecc561309532
Pankaj <tahiliani.pankaj19@gmail.com>
2020-11-06 13:20:40 -0800

    appending rows to the table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,31 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+tableData.forEach(element => {
+     var row = tbody.append("tr");
+
+    
+     Object.values(element).forEach(value => {
+        row.append("td").text(value);
+
+    });
+});
+
+var button = d3.select("#filter-btn");
+
+
+
+var inpfield = d3.select("#datetime");
+
+      
+tableData.forEach(element => {
+    if( element["datetime"]===inpfield){
+        Object.values(element).forEach(value =>{
+            row.append("td").text(value);
+        });
+        
+    };
+});
